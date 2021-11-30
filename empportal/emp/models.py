d6925588d8064e519d484990e626cfbb3ca8d4b7
sambasivarao.guduru <sambasivarao.guduru@ptw.com>
2021-11-30 09:39:38 +0530

    11/30 worked on models

--- empportal/emp/models.py
+++ empportal/emp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Emp(models.Model):
+    emp_type_choices =[('ADMIN','ADMIN'),('HR','HR'),('EMPLOYEE','EMPLOYEE')]
+    empid = models.CharField(max_length=15, unique=True)#varchar
+    name = models.CharField(max_length=250)
+    address=models.CharField(max_length=600, null=True, blank=True)
+    salary_perday = models.PositiveIntegerField(blank=True)
+    emp_type = models.CharField(choices=emp_type_choices, max_length=10) 
+    email = models.EmailField(max_length=250, blank=True)
+    phone = models.CharField(max_length=15, null=True)
+
+
+
