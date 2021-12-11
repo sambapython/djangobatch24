07ced5f0149c8ad1f92bdd6de6dfb3470c97942d
sambasivarao.guduru <sambasivarao.guduru@ptw.com>
2021-12-11 09:00:31 +0530

    12/11 manytomany relationship

--- sales/product/models.py
+++ sales/product/models.py
@@ -14,9 +14,28 @@ class Product(models.Model):
     cost = models.PositiveIntegerField(default=0)
     deliver_charges = models.PositiveIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    #many to one
     #category = models.CharField(max_length=250)
     # in this column we can add only category table id column values only
 
+class Customer(models.Model):
+    name = models.CharField(max_length=250)
+    # address = models.Textfield(max_length=650)
+    # phone = models.CharField(max_length=11)
+    # email = models.CharField(max_length=250)
+
 class Sales(models.Model):
-    pass
+    description = models.CharField(max_length=250)
+    products = models.ManyToManyField(Product, through='SalesProducts')
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class SalesProducts(models.Model):
+    sales = models.ForeignKey(Sales, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    cost = models.PositiveIntegerField(default=0)
+    quantity = models.PositiveIntegerField(default=1)
+
+
+
+    
 
